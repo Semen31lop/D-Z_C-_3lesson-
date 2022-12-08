353695b5653aff675a023ecb94749bec725af23e
Semen31lop <119083691+Semen31lop@users.noreply.github.com>
2022-12-09 02:19:38 +0300

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Write("Введите число x1: ");
+int x1= int.Parse(Console.ReadLine()!);
+Console.Write("Введите число x2: ");
+int x2= int.Parse(Console.ReadLine()!);
+Console.Write("Введите число y1: ");
+int y1= int.Parse(Console.ReadLine()!);
+Console.Write("Введите число y2: ");
+int y2= int.Parse(Console.ReadLine()!);
+Console.Write("Введите число z1: ");
+int z1= int.Parse(Console.ReadLine()!);
+Console.Write("Введите число z2: ");
+int z2= int.Parse(Console.ReadLine()!);
+int A=x1-x2; int B= y1-y2; int C=z1-z2;
+double length = Math.Sqrt(A*A+B*B+C*C);
+Console.WriteLine(length);
+
+
+
---
+++ Program23.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+int num=0;
+while (i < N)
+{
+    num = i * i * i;               
+    Console.WriteLine(num);
+    i++;
+}
